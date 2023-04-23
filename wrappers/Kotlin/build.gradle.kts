190412b3a20d3e9972565d1cd5cbd27c30495ae3
RealTriassic <techbyteofficial9@gmail.com>
2023-04-23 23:36:10 +0300

    feat: add kotlin wrapper

---
+++ wrappers/Kotlin/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
+tasks.shadowJar {
+    dependencies {
+        include(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
+    }
+}
+
+application {
+    mainClass.set("com.triassic.harbor.MainKt")
+}
---
+++ wrappers/Kotlin/src/main/kotlin/com/triassic/harbor/Main.kt
@@ -0,0 +1,26 @@
+package com.triassic.harbor
+
+import java.io.File
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+
+fun main() {
+    val url = URL("https://raw.githubusercontent.com/RealTriassic/Harbor/main/harbor.sh")
+    val destination = File("harbor.sh")
+
+    try {
+        downloadFile(url, destination)
+        val harbor = ProcessBuilder("sh", destination.name)
+        harbor.inheritIO()
+        harbor.start().waitFor()
+    } catch (e: Exception) {
+        println("Error downloading or running script: ${e.message}")
+        e.printStackTrace()
+    }
+}
+
+fun downloadFile(url: URL, destination: File) {
+    Files.copy(url.openStream(), Paths.get(destination.toURI()), StandardCopyOption.REPLACE_EXISTING)
+}
