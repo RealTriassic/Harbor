3254b31929a15883db161b34e9306923263456e0
RealTriassic <techbyteofficial9@gmail.com>
2023-04-24 20:46:52 +0300

    fix: improve python wrapper

--- wrappers/Python/Main.py
+++ wrappers/Python/Main.py
@@ -1,17 +1,21 @@
 import os
 import urllib.request
 
-url = "https://raw.githubusercontent.com/RealTriassic/Harbor/main/harbor.sh"
-destination = "harbor.sh"
+def main():
+    url = "https://raw.githubusercontent.com/RealTriassic/Harbor/main/harbor.sh"
+    destination = "harbor.sh"
 
-try:
-    # Download the script file
-    urllib.request.urlretrieve(url, destination)
+    try:
+        # Download the script file
+        urllib.request.urlretrieve(url, destination)
 
-    # Set executable permission on the downloaded file
-    os.chmod(destination, 0o755)
+        # Set executable permission on the downloaded file
+        os.chmod(destination, 0o755)
 
-    # Run the downloaded file
-    os.system(f"sh {destination}")
-except Exception as e:
-    print(f"Error downloading or running script: {e}")
+        # Run the downloaded file
+        os.system(f"sh {destination}")
+    except Exception as e:
+        print(f"Error downloading or running script: {e}")
+
+if __name__ == "__main__":
+    main()
